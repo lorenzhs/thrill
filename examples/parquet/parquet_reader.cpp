bce27fcbce461baaceda96b690f4c2054e89647b
Lorenz Hübschle-Schneider <huebschle@kit.edu>
2018-01-17 15:33:56 +0100

    Add a simple parquet example

---
+++ examples/parquet/parquet_reader.cpp
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * examples/parquet/parquet_reader.cpp
+ *
+ * A simple example that reads a single 32-bit integer column from a parquet
+ * file into a DIA.
+ *
+ * Part of Project Thrill - http://project-thrill.org
+ *
+ * Copyright (C) 2018 Lorenz Hübschle-Schneider <lorenz@4z2.de>
+ *
+ * All rights reserved. Published under the BSD-2 license in the LICENSE file.
+ ******************************************************************************/
+
+#include <thrill/api/parquet.hpp>
+#include <thrill/api/size.hpp>
+#include <thrill/common/logger.hpp>
+#include <tlx/cmdline_parser.hpp>
+
+#include <string>
+
+using namespace thrill; // NOLINT
+
+int main(int argc, char* argv[]) {
+
+    tlx::CmdlineParser clp;
+
+    std::string filename;
+    int column_index;
+    clp.add_param_string("filename", filename, "input filename");
+    clp.add_param_int("column", column_index, "column index");
+
+    if (!clp.process(argc, argv))
+        return -1;
+
+    return api::Run(
+        [filename, column_index](api::Context& ctx) {
+            auto num_values = ReadParquet<int32_t>(ctx, filename, column_index)
+                .Size();
+            sLOG1 << "Read" << num_values << "values from column"
+                  << column_index << "of" << filename;
+        });
+}
+
+/******************************************************************************/
